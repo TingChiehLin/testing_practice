6a7cfe1df2f193ecc624c09698d9458994924840
TingChiehLin <cooloojayoo@gmail.com>
2023-06-08 14:36:11 +1000

    Add - practice_1

--- src/1_basics.test.js
+++ src/1_basics.test.js
@@ -1,17 +1,21 @@
 import { multiply, isSmallerThan10 } from "./1_basics";
 
-describe("1_Basics", () => {
+describe("1_Basics Testing Practice", () => {
   describe("multiply()", () => {
     it("multiples a and b", () => {
       const result = multiply(3, 4);
-
       expect(result).toBe(12);
     });
   });
 
   describe("isSmallerThan10()", () => {
-    it("Your tests here...", () => {
-      // ...
+    it("should be smaller than 10?", () => {
+      const result = isSmallerThan10(7);
+      expect(result).toBe("yes dah!");
+    });
+    it("should not be smaller than 10?", () => {
+      const result = isSmallerThan10(12);
+      expect(result).toBe("not really...");
     });
   });
 });
