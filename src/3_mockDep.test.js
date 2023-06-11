48833c60f09cdf7c43ff30bc62bef08ecbed1760
TingChiehLin <cooloojayoo@gmail.com>
2023-06-11 16:52:56 +1000

    issues - testing with practice3

--- src/3_mockDep.test.js
+++ src/3_mockDep.test.js
@@ -1,9 +1,11 @@
 import { generatePassword } from "./3_mockDep";
 
 describe("3_mockDep_test", () => {
-  it("should", async () => {
-
-    const result = await generatePassword(5, "@");
-    // expect(result).toBe();
+  it("should give you an error when the size is 0", async () => {
+    await expect(() => generatePassword(0, "")).toThrowError();
+  });
+  it("should contain special character", async () => {
+    const result = await generatePassword(8, "@");
+    expect(result).toContain("@");
   });
 });
