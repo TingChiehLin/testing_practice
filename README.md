83cdd8937adbe6e128a0a6120be04dceec16977c
TingChiehLin <cooloojayoo@gmail.com>
2023-06-11 12:57:23 +1000

    Issue - mockup_test with 4

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 ## Learning Goal
 
--  
+learning hand-on test with Jest
+
+- Basic Test with toBe()
+- MockupFn
+- MockDep
+- MockFetch
+
+## Learning Resources
+
+- [How to Mock Fetch in Jest Manually](https://youtu.be/mHXhuPHiDj8)
+- [Mocking Fetch Using jest-fetch-mock](https://www.youtube.com/watch?v=yhUep7E9O20)
--- src/3_mockDep.test.js
+++ src/3_mockDep.test.js
@@ -1,20 +1,9 @@
 import { generatePassword } from "./3_mockDep";
 
 describe("3_mockDep_test", () => {
-  
-  //  const password = jest.fn(() => validatePasswordApi("@83XD310256"));
-  // it("should resolve if password is truely", async () => {
-  //   await expect(password).toBeTruthy();
-  // });
-  // it("shuld be smaller than 8 wrods", async () => {
-  //   await expect(password.length()).toBeLessThanOrEqual(8);
-  // });
-  // it("shuld include special character", async () => {
-  //   const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-  //   await expect(result).toMatch(specialChars);
-  // });
-  // it("should have random number < 10", async () => {
-  //   const result = await jest.fn(() => randomizer(1, 10));
-  //   expect(result).toBeLessThan(10);
-  // });
+  it("should", async () => {
+
+    const result = await generatePassword(5, "@");
+    // expect(result).toBe();
+  });
 });
